028e742caf7d85200f296e34cbbb2af9856d5eb0
Spencer Churchill <spencer.l.churchill@gmail.com>
2020-08-09 18:46:06 -0700

    update windows key

--- wallpaper.py
+++ wallpaper.py
@@ -65,7 +65,7 @@ print('Downloaded wallpaper')
 if sys == 'Darwin':
     system('osascript -e \'tell application "Finder" to set desktop picture to "'+img_path+'" as POSIX file\'')
 else:
-    key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER,"Control Panel\\Desktop",0,win32con.KEY_SET_VALUE)
+    key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel/Desktop", 0, win32con.KEY_SET_VALUE)
     win32api.RegSetValueEx(key, "WallpaperStyle", 0, win32con.REG_SZ, "0")
     win32api.RegSetValueEx(key, "TileWallpaper", 0, win32con.REG_SZ, "0")
     win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, img_path.replace('\\', '/'), 1+2)
