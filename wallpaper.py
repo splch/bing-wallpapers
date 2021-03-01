3db3c5d673b3a519b0103d97bf12dd5fcc3d0d29
Spencer Churchill <spencer.l.churchill@gmail.com>
2021-03-01 12:04:35 -0800

    fix python file

--- wallpaper.py
+++ wallpaper.py
@@ -1,76 +1,62 @@
-def main():
-    # Get current directory
-    folder = getcwd() + "/"
+from os import system, getcwd, path
+import requests
+import urllib.request
 
-    # URL in json format with latest wallpaper
-    url = "https://www.bing.com/HPImageArchive.aspx?format=js&n=1"
+from platform import system as _sys
+sys = _sys()
+print(sys)
 
-    # Get json output
-    resp = requests.get(url)
-    data = resp.json()
+if sys == 'Windows':
+    import win32api
+    import win32gui
+    import win32con
 
-    img = "https://www.bing.com" + data["images"][0]["url"]
+# Get current directory
+folder = getcwd()+'/'
 
-    img_path = folder + "daily_wallpaper" + img[-11:-7]
-    run = path.exists(img_path)  # check if first run
+print(folder)
 
-    try:
-        urllib.request.urlretrieve(img.replace("1080", "1200"), img_path)
-    except:
-        urllib.request.urlretrieve(img, img_path)
+# URL in json format with latest wallpaper
+url = 'https://www.bing.com/HPImageArchive.aspx?format=js&n=1'
 
-    # change wallpaper
-    if sys == "Darwin":
-        system(
-            'osascript -e \'tell application "System Events" to tell every desktop to set picture to "'
-            + img_path
-            + "\"'"
-        )
-    elif sys == "Linux":
-        system("gsettings set org.gnome.desktop.background picture-uri " + img_path)
-    elif sys == "Windows":
-        key = win32api.RegOpenKeyEx(
-            win32con.HKEY_CURRENT_USER,
-            "Control Panel/Desktop",
-            0,
-            win32con.KEY_SET_VALUE,
-        )
-        win32api.RegSetValueEx(key, "WallpaperStyle", 0, win32con.REG_SZ, "0")
-        win32api.RegSetValueEx(key, "TileWallpaper", 0, win32con.REG_SZ, "0")
-        win32gui.SystemParametersInfo(
-            win32con.SPI_SETDESKWALLPAPER, img_path.replace("\\", "/"), 1 + 2
-        )
+# Get json output
+resp = requests.get(url)
+data = resp.json()
 
-    # automatically change wallpaper
-    if not run:
-        if sys == "Darwin" or sys == "Linux":
-            system(
-                'crontab -l | { cat; echo "0 */2 * * * cd '
-                + folder
-                + " && $(which python3) "
-                + folder
-                + 'wallpaper.py"; } | crontab -'
-            )
-        elif sys == "Windows":
-            system(
-                'schtasks /create /sc hourly /mo 2 /tn "Daily Paper" /tr python3.exe '
-                + folder
-                + "wallpaper.py"
-            )
+# Form image url from json
 
+img = 'https://www.bing.com' + data['images'][0]['url']
 
-if __name__ == "__main__":
-    from os import system, getcwd, path
-    import requests
-    import urllib.request
+print(img)
 
-    from platform import system as _sys
+img_path = folder+'daily_wallpaper'+img[-11:-7]
+run = path.exists(img_path) # check if first run
 
-    sys = _sys()
+try:
+    urllib.request.urlretrieve(img.replace('1080', '1200'), img_path)
+except:
+    urllib.request.urlretrieve(img, img_path)
 
-    if sys == "Windows":
-        import win32api
-        import win32gui
-        import win32con
+print('Downloaded wallpaper')
 
-    main()
+# Set mac/win/linux wallpaper
+if sys == 'Darwin':
+    system('osascript -e \'tell application "System Events" to tell every desktop to set picture to "'+img_path+'"\'')
+elif sys == 'Linux':
+    system('gsettings set org.gnome.desktop.background picture-uri '+img_path)
+elif sys == 'Windows':
+    key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel/Desktop", 0, win32con.KEY_SET_VALUE)
+    win32api.RegSetValueEx(key, "WallpaperStyle", 0, win32con.REG_SZ, "0")
+    win32api.RegSetValueEx(key, "TileWallpaper", 0, win32con.REG_SZ, "0")
+    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, img_path.replace('\\', '/'), 1+2)
+
+print('Set wallpaper')
+
+# automatically change wallpaper
+if not run:
+    if sys == 'Darwin' or sys == 'Linux':
+        system('crontab -l | { cat; echo "0 */2 * * * cd '+folder+' && $(which python3) '+folder+'wallpaper.py"; } | crontab -')
+    elif sys == 'Windows':
+        system('schtasks /create /sc hourly /mo 2 /tn "Daily Paper" /tr python3.exe '+folder+'wallpaper.py')
+
+    print('Scheduled Daily Paper.')
